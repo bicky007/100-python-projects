1c2101867a2b7977d0d587e19d3c6a736a87b38b
Bicky <128511616+bicky007@users.noreply.github.com>
2024-02-04 23:50:35 +0530

    Update main.py

--- Treasure Island/main.py
+++ Treasure Island/main.py
@@ -21,7 +21,7 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
+print("Your mission is to find the treasure carefully.") 
 
 first = input("Left or right?\nType Right/Left: ").lower()
 if first == "right":
